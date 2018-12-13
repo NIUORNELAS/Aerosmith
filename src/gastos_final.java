d3eb0434d016af672621246e99211066765bdaf8
NIUORNELAS <noilsonornelas@hotmail.com>
2018-12-13 10:26:32 -0200

    E sobe o projeto aerosmith

---
+++ src/gastos_final.java
@@ -0,0 +1,15 @@
+
+public class gastos_final {
+
+	public static void main(String[] args) {
+		double gasto_janeiro=1488.01;
+		double gasto_fevereiro=2183.43;
+	double gasto_marco=3911.81;
+	double resultado=gasto_janeiro+gasto_fevereiro+gasto_marco;
+	System.out.println("resultado: " + resultado);
+	double media=resultado/3;
+	System.out.println("Com a média mensal de " + media);	
+int arredondado = (int) resultado;
+System.out.println("Resultado arredondado: " + arredondado)args;
+	}
+}
